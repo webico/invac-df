37ca0edab912208f68c6ca5f9c80a6c8ebe003aa
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-04-13 19:48:18 -0300

    feat: finalizado o css do component Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,21 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <header className="header">
+    <header className={`header ${menu ? '--open' : ''}`}>
       <div className="header__container">
 
         <div className="header-logo">
           <Link href='#'><img src='/logo.svg' /></Link>
-          <button id='header__button'>menu</button>
+          <button className='header__button' onClick={() => setMenu(!menu)}>menu</button>
         </div>
 
-        <div id="menu">
-          <nav className="menu__nav">
-            <ul className="menu-list">
+        <div className="menu">
+          <nav>
+            <ul>
               <li><Link href='#' className="menu-list__item">Página Inicial</Link></li>
               <li><Link href='#' className="menu-list__item">Agendamento</Link></li>
               <li><Link href='#' className="menu-list__item">Consulta</Link></li>
@@ -25,10 +28,10 @@ const Navbar = () => {
             <button>Instalar Aplicativo</button>
           </div>
 
-          <div className="menu__info">
-            <ul className="menu-list">
+          <div>
+            <ul className="menu__info__div">
               <li><Link href='#' className='menu-info__item'>Entre em contato</Link></li>
-              <li><Link href='#' className='menu-info__item'>Acesso À informação</Link></li>
+              <li><Link href='#' className='menu-info__item'>Acesso à Informação</Link></li>
               <li><Link href='#' className='menu-info__item'>Política de Privacidade</Link></li>
             </ul>
           </div>
