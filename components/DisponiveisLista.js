022a44e0cde5f0441801e7adf53e6c72bc19dfb3
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-04-14 22:55:00 -0300

    refactor: componentização da seção de disponibilidade para limpeza do código

---
+++ components/DisponiveisLista.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const DisponiveisLista = ({ props }) => {
+  DisponiveisLista.propTypes = {
+    props: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.array]),
+    disponibilidade: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.array])
+  };
+
+  const vacinas = props.disponibilidade.vacinas,
+    testes = props.disponibilidade.testes;
+
+  const [data, setData] = useState(vacinas),
+    [nome, setNome] = useState('vacinas'),
+    [itemsShow, setItemsShow] = useState(4);
+
+  function handleChange(e) {
+    //dá pra melhorar isso aqui, tá feio demais
+    if (e.target.value == 'testes') {
+      setData(testes);
+      setNome('testes');
+    } else {
+      setData(vacinas);
+      setNome('vacinas');
+    }
+  }
+
+  function handleShowMore() {
+    setItemsShow(data.length);
+  }
+
+  return (
+    <section className='disponivel'>
+      <div className="container">
+        <div className="disponivel__filtro">
+          <p>Disponibilidade de:</p>
+
+          <select name="" id="" onClick={handleChange}>
+            <option value="vacinas">Vacinas</option>
+            <option value="testes">Testes</option>
+          </select>
+        </div>
+
+        <ul className='disponivel__lista'>
+          {data && data.slice(0, itemsShow).map(regiao =>
+            <li key={regiao.id} className='disponivel__card'>
+              <div className='card__info'>
+                <span className='regiao'>Região</span>
+                <h2 className='regiao__nome'>{regiao.nome_regiao}</h2>
+                <p className='regiao__disponiveis'>{regiao.quantidade} {nome} disponíveis</p>
+              </div>
+
+              <button className='card__btn'></button>
+            </li>
+          )}
+        </ul>
+
+        {itemsShow == 4 ? <button className='ver-todas' onClick={handleShowMore}>Ver todas as regiões</button> : ''}
+      </div>
+    </section>
+  );
+};
+
+export default DisponiveisLista;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import fsPromises from 'fs/promises';
 import path from 'path';
 import { useState } from 'react';
+import DisponiveisLista from '@/components/DisponiveisLista';
 
 export async function getStaticProps() {
   // eslint-disable-next-line no-undef
@@ -27,28 +28,6 @@ export default function Home(props) {
       PropTypes.array])
   };
 
-  const vacinas = props.disponibilidade.vacinas,
-    testes = props.disponibilidade.testes;
-
-  const [data, setData] = useState(vacinas),
-    [nome, setNome] = useState('vacinas'),
-    [itemsShow, setItemsShow] = useState(4);
-
-  function handleChange(e) {
-    //dá pra melhorar isso aqui, tá feio demais
-    if (e.target.value == 'testes') {
-      setData(testes);
-      setNome('testes');
-    } else {
-      setData(vacinas);
-      setNome('vacinas');
-    }
-  }
-
-  function handleShowMore() {
-    setItemsShow(data.length);
-  }
-
   return (
     <>
       <Head>
@@ -66,34 +45,7 @@ export default function Home(props) {
         </div>
       </main>
 
-      <section className='disponivel'>
-        <div className="container">
-          <div className="disponivel__filtro">
-            <p>Disponibilidade de:</p>
-
-            <select name="" id="" onClick={handleChange}>
-              <option value="vacinas">Vacinas</option>
-              <option value="testes">Testes</option>
-            </select>
-          </div>
-
-          <ul className='disponivel__lista'>
-            {data && data.slice(0, itemsShow).map(regiao =>
-              <li key={regiao.id} className='disponivel__card'>
-                <div className='card__info'>
-                  <span className='regiao'>Região</span>
-                  <h2 className='regiao__nome'>{regiao.nome_regiao}</h2>
-                  <p className='regiao__disponiveis'>{regiao.quantidade} {nome} disponíveis</p>
-                </div>
-
-                <button className='card__btn'></button>
-              </li>
-            )}
-          </ul>
-
-          {itemsShow == 4 ? <button className='ver-todas' onClick={handleShowMore}>Ver todas as regiões</button> : ''}
-        </div>
-      </section>
+      <DisponiveisLista props={props} />
 
       <section className='consulta'>
         <div className="container">
