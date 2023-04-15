17da9fc94299407dcd277e370836dc5d6bc64f82
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-04-14 22:40:14 -0300

    feat: selçao de vacinas e testes funcionando com botão de 'ver todas as regioes'

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,8 @@ export default function Home(props) {
     testes = props.disponibilidade.testes;
 
   const [data, setData] = useState(vacinas),
-    [nome, setNome] = useState('vacinas');
+    [nome, setNome] = useState('vacinas'),
+    [itemsShow, setItemsShow] = useState(4);
 
   function handleChange(e) {
     //dá pra melhorar isso aqui, tá feio demais
@@ -42,6 +43,10 @@ export default function Home(props) {
       setData(vacinas);
       setNome('vacinas');
     }
+  }
+
+  function handleShowMore() {
+    setItemsShow(data.length);
   }
 
   return (
@@ -66,14 +71,14 @@ export default function Home(props) {
           <div className="disponivel__filtro">
             <p>Disponibilidade de:</p>
 
-            <select name="" id="" onChange={handleChange}>
+            <select name="" id="" onClick={handleChange}>
               <option value="vacinas">Vacinas</option>
               <option value="testes">Testes</option>
             </select>
           </div>
 
           <ul className='disponivel__lista'>
-            {data && data.map(regiao =>
+            {data && data.slice(0, itemsShow).map(regiao =>
               <li key={regiao.id} className='disponivel__card'>
                 <div className='card__info'>
                   <span className='regiao'>Região</span>
@@ -85,6 +90,8 @@ export default function Home(props) {
               </li>
             )}
           </ul>
+
+          {itemsShow == 4 ? <button className='ver-todas' onClick={handleShowMore}>Ver todas as regiões</button> : ''}
         </div>
       </section>
     </>
