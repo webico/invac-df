8b8682a9871fa2d4fd9ca21414f7b6682aa70c12
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-05-29 21:03:26 -0300

    feat: links atualizados no footer e navbar

--- components/UserHelpLinks.js
+++ components/UserHelpLinks.js
@@ -1,13 +1,11 @@
-import Link from 'next/link';
-
 const UserHelpLinks = () => {
   return (
     <>
       <ul className="menu_info">
-        <li><Link href='#' className='menu_info__item'>Ouvidoria</Link></li>
-        <li><Link href='#' className='menu_info__item'>Entre em contato</Link></li>
-        <li><Link href='#' className='menu_info__item'>Acesso à Informação</Link></li>
-        <li><Link href='#' className='menu_info__item'>Política de Privacidade</Link></li>
+        <li><a href="http://www.ouvidoria.df.gov.br/" target="_blank" rel="noopener noreferrer" className='menu_info__item'>Ouvidoria</a></li>
+        <li><a href="https://www.saude.df.gov.br/fale-com-a-secretaria" target="_blank" rel="noopener noreferrer" className='menu_info__item'>Entre em contato</a></li>
+        <li><a href="https://www.df.gov.br/category/acesso-a-informacao/" target="_blank" rel="noopener noreferrer" className='menu_info__item'>Acesso à Informação</a></li>
+        <li><a href="https://info.saude.df.gov.br/politica-de-privacidade/" target="_blank" rel="noopener noreferrer" className='menu_info__item'>Política de Privacidade</a></li>
       </ul>
 
       <p className="copyright">SESDF Invac DF © 2023</p>
